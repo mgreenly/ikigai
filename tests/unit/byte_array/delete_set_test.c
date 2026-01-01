6b2417f01a002f1066b570442e733fb0b2155ba0
ai4mgreenly <ai4mgreenly@logic-refinery.com>
2026-01-01 02:00:26 -0600

    refactor: remove dead code ik_byte_array_set
    harness/prune | sonnet:think

--- tests/unit/byte_array/delete_set_test.c
+++ tests/unit/byte_array/delete_set_test.c
@@ -89,32 +89,6 @@ START_TEST(test_byte_array_delete_from_end) {
 }
 
 END_TEST
-// Test set byte
-START_TEST(test_byte_array_set) {
-    TALLOC_CTX *ctx = talloc_new(NULL);
-
-    res_t res = ik_byte_array_create(ctx, 10);
-    ck_assert(is_ok(&res));
-    ik_byte_array_t *array = res.ok;
-
-    // Add [0, 1, 2]
-    for (uint8_t i = 0; i < 3; i++) {
-        res = ik_byte_array_append(array, i);
-        ck_assert(is_ok(&res));
-    }
-
-    // Set middle element to 99
-    ik_byte_array_set(array, 1, 99);
-
-    // Verify: [0, 99, 2]
-    ck_assert_uint_eq(ik_byte_array_get(array, 0), 0);
-    ck_assert_uint_eq(ik_byte_array_get(array, 1), 99);
-    ck_assert_uint_eq(ik_byte_array_get(array, 2), 2);
-
-    talloc_free(ctx);
-}
-
-END_TEST
 
 #if !defined(NDEBUG) && !defined(SKIP_SIGNAL_TESTS)
 // Test assertion on get with out of bounds index
@@ -138,19 +112,6 @@ START_TEST(test_byte_array_delete_out_of_bounds_asserts) {
     ik_byte_array_t *array = res.ok;
 
     ik_byte_array_delete(array, 0); // Empty array - should assert
-
-    talloc_free(ctx);
-}
-
-END_TEST
-// Test assertion on set with out of bounds index
-START_TEST(test_byte_array_set_out_of_bounds_asserts) {
-    TALLOC_CTX *ctx = talloc_new(NULL);
-
-    res_t res = ik_byte_array_create(ctx, 10);
-    ik_byte_array_t *array = res.ok;
-
-    ik_byte_array_set(array, 0, 99); // Empty array - should assert
 
     talloc_free(ctx);
 }
@@ -184,9 +145,6 @@ static Suite *byte_array_delete_set_suite(void)
     tcase_add_test(tc_core, test_byte_array_delete_from_middle);
     tcase_add_test(tc_core, test_byte_array_delete_from_end);
 
-    // Set test
-    tcase_add_test(tc_core, test_byte_array_set);
-
     suite_add_tcase(s, tc_core);
 
 #if !defined(NDEBUG) && !defined(SKIP_SIGNAL_TESTS)
@@ -199,7 +157,6 @@ static Suite *byte_array_delete_set_suite(void)
     tcase_set_timeout(tc_assertions, 30); // Longer timeout for valgrind
     tcase_add_test_raise_signal(tc_assertions, test_byte_array_get_out_of_bounds_asserts, SIGABRT);
     tcase_add_test_raise_signal(tc_assertions, test_byte_array_delete_out_of_bounds_asserts, SIGABRT);
-    tcase_add_test_raise_signal(tc_assertions, test_byte_array_set_out_of_bounds_asserts, SIGABRT);
     tcase_add_test_raise_signal(tc_assertions, test_byte_array_insert_out_of_bounds_asserts, SIGABRT);
     suite_add_tcase(s, tc_assertions);
 #endif
