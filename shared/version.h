b75c4cd2458fc7b99f161e95c6e9bb3972d8bd47
ai4mgreenly <ai4mgreenly@logic-refinery.com>
2026-03-14 19:47:17 -0500

    Goal #479: Update version to 0.14.0 and README install tag to rel-13

--- shared/version.h
+++ shared/version.h
@@ -2,9 +2,9 @@
 #define IK_VERSION_H
 
 #define IK_VERSION_MAJOR 0
-#define IK_VERSION_MINOR 13
+#define IK_VERSION_MINOR 14
 #define IK_VERSION_PATCH 0
 
-#define IK_VERSION "0.13.0"
+#define IK_VERSION "0.14.0"
 
 #endif // IK_VERSION_H
